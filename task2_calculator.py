4ed46b7c27920381cc3f5721e9b9434cefbce9e7
thakuranubhavkumar1710-ship-it <thakuranubhavkumar1710@gmail.com>
2026-09-04 16:33:15 +0530

    Add files via upload

---
+++ task2_calculator.py
@@ -0,0 +1,21 @@
+# task2_calculator.py
+def calculator():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    op = input("Choose operation (+, -, *, /): ")
+
+    if op == "+":
+        print("Result:", num1 + num2)
+    elif op == "-":
+        print("Result:", num1 - num2)
+    elif op == "*":
+        print("Result:", num1 * num2)
+    elif op == "/":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Division by zero!")
+    else:
+        print("Invalid operation!")
+
+calculator()
